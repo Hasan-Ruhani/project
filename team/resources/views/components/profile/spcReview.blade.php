4f6583e41afc6fe6441ed5515ae7fc0d43039dea
H A S A N <hasan25042019@gmail.com>
2023-12-22 22:53:59 +0600

    tt

--- team/resources/views/components/profile/spcReview.blade.php
+++ team/resources/views/components/profile/spcReview.blade.php
@@ -2,13 +2,13 @@
 <section id="testimonials" class="testimonials">
     <div class="container" data-aos="zoom-in">
 
-      <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-        <div class="swiper-wrapper">
+      {{-- <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100"> --}}
+        <div id="reviewList" class="swiper-wrapper">
 
-          <div id="reviewList" class="swiper-slide">
+          
             
-          </div><!-- End testimonial item -->
-        </div>
+          <!-- End testimonial item -->
+        {{-- </div> --}}
         <div class="swiper-pagination"></div>
       </div>
 
@@ -17,37 +17,34 @@
 
 
   <script>
-            
-        spcReview(5);
- 
-        function spcReview() {
-            let searchParams = new URLSearchParams(window.location.search);
-            let id = searchParams.get('id');
-            console.log("id:", id);
+          
+        async function spcReview() {
+          let searchParams = new URLSearchParams(window.location.search);
+          let id = searchParams.get('id');
+          console.log("id:", id);
 
-            axios.get("/spcUserReview/" + id)
-                .then((res) => {
-                    let user = res.data[0].user;
-                    console.log("info:", user);
+          let res = await axios.get("/spcUserReview/" + id);
 
-                    res.data.forEach((item, i) => {
-                        let EachItem = `<div class="testimonial-item">
-                            <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                            <h3>${user.name}</h3>
-                            <h4>${user.email}</h4>
-                            <p>
-                                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                ${item.review}
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                            </p>
-                        </div>`;
-                        $("#reviewList").append(EachItem);
-                    });
-                })
-                .catch((error) => {
-                    console.error("Error fetching user review data:", error);
-            });
+          res.data.forEach((item, i) => {
+              let user = item.user; // Access user data from the current item
+              console.log("info:", user);
+              let EachItem = `<div class="swiper-slide">
+                  <div class="testimonial-item">
+                      <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
+                      <h3>${user.name}</h3>
+                      <h4>${user.email}</h4>
+                      <p>
+                          <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                          ${item.review}
+                          <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                      </p>
+                  </div>
+              </div>`;
+              $("#reviewList").append(EachItem);
+          });
         }
+        spcReview();
+
 
 
     </script>
