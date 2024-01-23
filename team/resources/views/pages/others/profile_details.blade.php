e266ea25709acd84c4a72946c6089fc4c333d881
H A S A N <hasan25042019@gmail.com>
2024-01-23 22:02:17 +0600

    cx

--- team/resources/views/pages/others/profile_details.blade.php
+++ team/resources/views/pages/others/profile_details.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     @include('components.header2')
     @include('components.profile.profile_details')
-    @include('components.profile.spcReview')
-    @include('components.profile.createReview')
+    {{-- @include('components.profile.spcReview')
+    @include('components.profile.createReview') --}}
     @include('components.profile.spcContact')
     @include('components.footer')
 @endsection
